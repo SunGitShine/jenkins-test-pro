78385957defa61219c66b2568768e3b0bc0e7702
SunShine <489649421@qq.com>
2017-09-06 10:06:17 +0800

    xx

---
+++ src/main/java/com/yyfz/jenkins/App.java
@@ -0,0 +1,13 @@
+package com.yyfz.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
